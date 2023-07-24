d341c81bc5fa1febcca973b3c0ea09513446ed1c
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2023-07-23 22:10:01 -0300

    :zap: Criação da classe photo

--- model/photoModel.php
+++ model/photoModel.php
@@ -13,10 +13,87 @@ class Address extends Connection {
         $this->id=$id;
         return $this;
     }
+    public function setName($name){
+        $this->name=$name;
+        return $this;
+    }
+    public function setSequence($sequence){
+        $this->sequence=$sequence;
+        return $this;
+    }
+    public function setCreated($created){
+        $this->created=$created;
+        return $this;
+    }
+    public function setUpdated($updated){
+        $this->updated=$updated;
+        return $this;
+    }
+    public function setIdProducts($id_products){
+        $this->id_products=$id_products;
+        return $this;
+    }
 
     public function getId(){
         return $this->id;
     }
-}
+    public function getName(){
+        return $this->name;
+    }
+    public function getSequence(){
+        return $this->sequence;
+    }
+    public function getCreated(){
+        return $this->created;
+    }
+    public function getUpdated(){
+        return $this->updated;
+    }
+    public function getIdProducts(){
+        return $this->id_products;
+    }
 
+    function photo_save(){
+         $sql_query = "SELECT * FROM photo_insert_function
+                        (
+                            '" . $this->getName() . "',
+                            '" . $this->getSequence() . "',
+                            '" . $this->getCreated() . "',
+                            '" . $this->getUpdated() . "',
+                            '" . $this->getIdProducts() . "'
+                        )";
+        $pdo = $this->o_db;
+        $stmt = $pdo->prepare($sql_query);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        return $row;
+    }
+
+    function post_photo_save(){
+        $result = $this->photo_save();
+        return $result;
+    }
+
+    function photo_list(){
+        $sql_query = "SELECT * FROM view_photo_products WHERE id_products = '" . $this->getIdProducts() . "'";
+        $pdo = $this->o_db;
+        $stmt = $pdo->prepare($sql_query);
+        $array_photo= array();
+        $stmt->execute();
+        while($row = $stmt->fetch()){
+            $the_photo = new Photo();
+            $the_photo->setId($row[0]);
+            $the_photo->setName($row[1]);
+            $the_photo->setSequence($row[2]);
+            array_push($array_photo, $the_photo);
+        }
+        return $array_photo;
+    }
+
+    function post_photo_list(){
+        $result = $this->photo_list();
+        return $result;
+    }
+
+}
 ?>
---
+++ view/photo_new.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.min.css" crossorigin="anonymous">
+    <title>Library</title>
+    <style>
+        #main_area {
+            margin-top: 50px;
+        }
+    </style>
+  </head>
+  <body>
+  <?php require_once 'menu.php'; ?>
+  <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center col-md-8 order-md-1">
+  <div class="card">
+    <div class="card-body">
+      <form name="register" action="<?php echo URLROOT; ?>/photo/save/<?php echo $stock_keeping_unit; ?>" method="post" enctype="multipart/form-data">
+        <div class="form-row">
+          <div class="input-group col-md-12">
+            <label for="exampleFormControlFile1">Foto</label>     
+            <input type="file" class="form-control-file" id="inputPhoto" name="photo" accept="image/png, image/jpeg">
+          </div>
+        </div>
+        <br><a class="btn btn-outline-primary" href="<?php echo URLROOT; ?>/products/edit/<?php echo $stock_keeping_unit; ?>" role="button">Cancelar</a>
+        <button type="submit" class="btn btn-primary">Carregar</button> 
+      </form>
+    </div>
+    </div>
+</div>
+<script type="text/javascript" src="<?php echo URLROOT; ?>/js/jquery-3.5.1.slim.min.js"></script>
+<script src="<?php echo URLROOT; ?>/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
