4ca9268f9b944c7a27413636573057c2c6459933
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2024-02-14 21:06:28 -0300

    :memo: Ajuste de footer e adição de campos

--- view/products_edit.php
+++ view/products_edit.php
@@ -29,7 +29,7 @@
         transition: border .2s ease-in-out;
     }
     </style>
-    
+    <script src="https://kit.fontawesome.com/14edfcae2f.js" crossorigin="anonymous"></script>
   </head>
   <body>
   <?php require_once 'menu.php'; ?>
@@ -51,13 +51,24 @@ function Mask($mask,$str){
 ?>
 <form action="<?php echo URLROOT; ?>/products/update/<?php echo $products->getStockKeepingUnit(); ?>" method="post">
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-5">
       <label for="inputSku">Stock Keeping Unit</label>
       <input type="text" class="form-control" id="inputSku" name="sku" value="<?php echo $products->getStockKeepingUnit(); ?>" maxlength="100" readonly>
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-7">
       <label for="inputSku">Descrição/Nome do produto</label>
       <input type="text" class="form-control" id="inputDescricao" name="descricao" value="<?php echo $products->getDescription(); ?>" maxlength="100">
+      </div>
+    </div>
+
+    <div class="form-row">
+    <div class="form-group col-md-3">
+      <label for="inputSkux">Marca</label>
+      <input type="text" class="form-control" id="inputSkux" name="skux" value="" maxlength="100" readonly>
+    </div>
+    <div class="form-group col-md-9">
+      <label for="inputSkux">Categoria</label>
+      <input type="text" class="form-control" id="inputDescricaox" name="descricaox" value="" maxlength="100" readonly>
       </div>
     </div>
 
@@ -73,7 +84,7 @@ foreach($photo as &$photo_value):
           <center><a href="<?php echo URLROOT; ?>/foto/<?php echo $products->getStockKeepingUnit(); ?>/<?php echo $photo_value->getFileName()."?t=".strtotime($photo_value->getCreated()); ?>" target="_blank"><img src="<?php echo URLROOT; ?>/foto/<?php echo $products->getStockKeepingUnit(); ?>/<?php echo $photo_value->getFileNameThumbnail()."?t=".strtotime($photo_value->getCreated()); ?>" alt="Image preview" class="thumbnail" width="120" height="120"></a></center>
         </div>
         <div class="form-group col-md-2">
-            <label for="inputSku">Nome do arquivo</label>
+            <label for="inputSku">Nome do arquivo <?php if($photo_value->getSequence() == 1){ ?><i title="Foto principal!" class="fa-solid fa-flag" style="color: #FFD43B;"></i><?php } ?></label>
             <input type="text" class="form-control" id="inputSku" name="sku" value="<?php echo $photo_value->getFileName(); ?>" maxlength="100" readonly>
         </div>
         <div class="form-group col-md-1">
@@ -138,8 +149,8 @@ foreach($video as &$video_value):
     </div>
     <div class="form-row">
         <div class="form-group col-md-12">
-            <label for="inputSku">URL do Vídeo MP4</label>
-            <input type="text" class="form-control" id="inputSku" name="sku" value="<?php echo "https://" . $_SERVER['SERVER_NAME']; ?><?php echo URLROOT; ?>/video/<?php echo $video_value->getFileName(); ?>" maxlength="100" readonly>
+            <label for="inputSku">URL do Vídeo MP4 <i id="copyButton" class="fa-solid fa-copy"></i></label>
+            <input type="text" class="form-control" id="urlVideo" name="urlVideo" value="<?php echo "https://" . $_SERVER['SERVER_NAME']; ?><?php echo URLROOT; ?>/video/<?php echo $video_value->getFileName(); ?>" maxlength="100" readonly>
         </div>
     </div>
 
@@ -155,9 +166,25 @@ endforeach;
 </form>
 </div>
 </div>
+<footer class="pt-3 my-md-2">
+      <div class="row">
+        <div class="col-12 col-md">
+          <small class="d-block mb-3 text-muted text-center">© 2024<br><a href="https://github.com/douglascarlos-dev/Product-Library" target="_blank"><img src="<?php echo URLROOT; ?>/img/logo.svg" alt="HTML tutorial" style="height:35px;"></a></small>
+        </div>
+      </div>
+    </footer>
 </div>
 
 <script type="text/javascript" src="<?php echo URLROOT; ?>/js/jquery-3.5.1.slim.min.js"></script>
 <script src="<?php echo URLROOT; ?>/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function() {
+  $('#copyButton').click(function() {
+    var copyText = $('#urlVideo');
+    copyText.select();
+    document.execCommand('copy');
+  });
+});
+</script>
 </body>
 </html>
