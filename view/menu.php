bc8656ccb51125ab8925434270c7c9765466d9b2
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2024-02-14 21:08:17 -0300

    :zap: Melhoria de busca de produto

--- view/menu.php
+++ view/menu.php
@@ -2,8 +2,9 @@
   <h5 class="my-0 mr-md-auto font-weight-normal">Library</h5>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/">Inicio</a>
-    <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/products">Produtos</a>
+    <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/products/visualizar">Produtos</a>
     <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/documents">Arquivos</a>
+    <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/banner/visualizar">Banner</a>
     <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/settings/edit">Configurações</a>
     <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/">Sobre</a>
     <a class="p-2 text-dark" href="<?php echo URLROOT; ?>/user/logout">Sair</a>
